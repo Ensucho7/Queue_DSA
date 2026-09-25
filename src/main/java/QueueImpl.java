96880e98fbcaf9a60b77da572843d02606f6d268
Enzo <ensucho77@gmail.com>
2026-09-25 13:21:43 +0200

    New classes isFull, isEmpty and pop on QueueImpl.java. All working well and 100% coverage with tests.

--- src/main/java/QueueImpl.java
+++ src/main/java/QueueImpl.java
@@ -7,14 +7,12 @@ public class QueueImpl<E> implements Queue<E>{
     private int p;
 
     public QueueImpl(int len) {
-        // TO-DO
         this.data = (E[])new Object[len];
         logger.info("nova cua de " + len+" elements");
 
     }
 
     public void push(E e) throws FullQueueException {
-        // TO-DO
         logger.info("pre: 'push' nou element "+ e);
         if (isFull()) {
             logger.error("Cua plena");
@@ -28,24 +26,33 @@ public class QueueImpl<E> implements Queue<E>{
 
 
     public E pop() throws EmptyQueueException {
-        // TO-DO
         logger.info("pre: 'pop' nou element ");
-        return null;
+        if (isEmpty()) {
+            logger.error("Cua buida");
+            throw new EmptyQueueException();
+        }
+
+        E element = this.data[0];
+        
+        for (int i = 0; i < this.p - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        this.data[this.p - 1] = null;
+        this.p--;
+        
+        logger.info("post: element extret " + element);
+        return element;
     }
 
     private boolean isFull() {
-        // TO-DO
-        //return this.p == this.data.length;
-        return false;
+        return this.p == this.data.length;
     }
 
     private boolean isEmpty() {
-        // TO-DO
-        return false;
+        return this.p == 0;
     }
 
     public int size() {
-        //TO-DO
         return this.p;
     }
 }
